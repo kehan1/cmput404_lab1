c995aa79cbf27b3b45dc88b98178e0b1380caebb
301git <kehan1@ualberta.ca>
2019-01-10 17:51:27 -0700

    lab1 modified

--- lab1.py
+++ lab1.py
@@ -4,3 +4,6 @@ print(requests.__version__)
 r = requests.get("http://www.google.com")
 print(r.status_code)
 print(r.text)
+
+a  = requests.get("https://raw.githubusercontent.com/kehan1/cmput404_lab1/master/lab1.py")
+print(a.text)
